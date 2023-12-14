90a743a7e92e2ad207f5809ef2cf51e8c4dfad00
AdamKav-student <123458911+AdamKav-student@users.noreply.github.com>
2023-12-14 12:04:44 +0000

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,55 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Not Tetris");
+
+	sf::Event event;
+
+	while (window.isOpen()) {
+
+		while (window.pollEvent(event)) {
+
+			if (event.type == sf::Event::Closed) {
+
+				window.close();
+			}
+		}
+	}
+
+	return 0;
+}
+
+/*
+void fileToString(string fname, string data)
+{
+    //load file
+    ifstream fin(fname);
+
+    //check if file is open
+    if (fin)
+    {
+        //read file into string
+        string line;
+        //while there is a line to read
+        while (getline(fin, line))
+        {
+            //add line to data
+            data += line + "\n";
+        }
+        fin.close();
+    }
+    //if file is not open
+    else
+    {
+        cout << "Error opening file" << endl;
+    }
+}
+int main()
+{
+    string file;
+    fileToString("vs_sample_simple.xml", file);
+
+    return 1;
+}
+*/
